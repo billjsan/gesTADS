3039d68ca6e3708d50f1e13f8b32e7ef8ba6524b
bill <wjs@discente.ifpe.edu.br>
2022-07-23 12:14:18 -0300

    TEST -
    - cria um artifacts para o pacote test
    - testando funcionalidade de limpar a saída do console

--- test/GesTADSTest.java
+++ test/GesTADSTest.java
@@ -4,6 +4,46 @@ public class GesTADSTest {
 
     public static void main(String[] args) {
 
-        ScreenInputTest inputTest = new ScreenInputTest();
+        //ScreenInputTest inputTest = new ScreenInputTest();
+
+        sujaATela();
+
+        esperaEmSeg(1);
+
+        ScreenInputTest.apagaScreen();
+
+        esperaEmSeg(1);
+
+        sujaATela();
+
+        esperaEmSeg(1);
+
+        ScreenInputTest.apagaScreen();
+    }
+
+    private static void sujaATela() {
+        System.out.println("sujei a tela");
+        System.out.println("sujei a tela");
+        System.out.println("sujei a tela");
+        System.out.println("sujei a tela");
+        System.out.println("sujei a tela");
+        System.out.println("sujei a tela");
+        System.out.println("sujei a tela");
+        System.out.println("sujei a tela");
+        System.out.println("sujei a tela");
+        System.out.println("sujei a tela");
+        System.out.println("sujei a tela");
+        System.out.println("sujei a tela");
+        System.out.println("sujei a tela");
+        System.out.println("sujei a tela");
+    }
+
+    private static void esperaEmSeg(int segundos) {
+        int timeImMillis = segundos * 1000;
+        try {
+            Thread.sleep(timeImMillis);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
