6a3ad1f6cf30a5377fd6fd17b51f9d562d5e3812
Izavan Correia <isc1@discente.ifpe.edu.br>
2022-08-09 15:50:55 -0300

    WIP -
    - AdiÃÃo dos mÃtodos de produto

--- README.md
+++ README.md
@@ -11,3 +11,17 @@
 
 * <h3>Diagrama de casos de uso</h3>
 <img src="doc/img/usecaseDiagram.jpeg" alt="basic architecture" height="725">
+
+<h3 align= 'center'>Banco de dados</h3>
+<h5 align=center>Esse projeto utiliza um banco de dados MySQL, dessa forma é necessário criar o banco de dados e as tabelas antes de utilizar o sistema.</h5>
+
+<h5>Passo 1: verificar se o MySQL está instalado na máquina que vai rodar o sistema gesTADS. Caso esteja instalado entre no terminal do MySQL com as credenciais root para usuário e não informar nada para a senha. Caso não tenha o MySQL instalado na máquina fazer a instalação do mesmo e refazer esse passo.</h5>
+<h5>Passo 2: criar o banco de dados através do comando SQL: create database gesTADS;</h5>
+<h5>Passo 3: Entrar no banco de dados criado anteriormente através do comando SQL: use gesTADS;</h5>
+<h5>Passo 4: Criar as tabelas do banco de dados. São elas: employee, cargo, produto e transacao. Para criar as tabelas utilize os comandos SQL a seguir: create table employee (id int primary key, nome varchar (255), login varchar (255), senha varchar (255), cargo varchar (255), cpf varchar (100) not null unique); create table cargo (id int auto_increment primary key, nome varchar (255));
+create table produto (id int primary key, nome varchar (255), serialn varchar (255), fabricante varchar (255), descricao varchar (255), fabricacao varchar (255), validade varchar (255));
+</h5>
+<h5>Passo 5: verificar se as tabelas foram criadas através do comando SQL: show tables;</h5>
+<h5>Pronto, seu banco de dados está configurado e pronto para uso!</h5>
+
+
--- src/model/repository/database/PersistDatabase.java
+++ src/model/repository/database/PersistDatabase.java
@@ -11,7 +11,6 @@ import java.util.List;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
-
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 
@@ -69,22 +68,25 @@ public class PersistDatabase implements GesTADSDataBaseInterface {
     }
 
     public void insertEmployee(Employee employee){
-        String sql = "insert into employee(nome, login, senha, cargo, cpf) values (?,?,?,?,?)";
+        String sql = "insert into employee(id, nome, login, senha, cargo, cpf) values (?,?,?,?,?,?)";
 
         try {
             PreparedStatement pstm = src.model.repository.database.ConnectionDataBase.getCurrentConnection().prepareStatement(sql);
 
-            pstm.setString(1,employee.getNome());
-            pstm.setString(2,employee.getLogin());
-            pstm.setString(3,employee.getSenha());
-            pstm.setString(4,employee.getCargo());
-            pstm.setString(5,employee.getCpf());
+            pstm.setLong(1,employee.getId());
+            pstm.setString(2,employee.getNome());
+            pstm.setString(3,employee.getLogin());
+            pstm.setString(4,employee.getSenha());
+            pstm.setString(5,employee.getCargo());
+            pstm.setString(6,employee.getCpf());
 
             pstm.execute();
 
         } catch (SQLException e) {
             if(GesLogger.ISFULLLOGABLE || GesLogger.ISERRORLOGABLE)
                 GesLogger.e(TAG, "Erro ao inserir employee: " + e.getMessage());
+        } catch (Exception e){
+            System.out.println(e);
         }
     }
 
@@ -100,7 +102,6 @@ public class PersistDatabase implements GesTADSDataBaseInterface {
             while(rs.next()) {
 
                 Employee e = new Employee();
-
                 e.setNome(rs.getString("nome"));
                 e.setLogin(rs.getString("login"));
                 e.setSenha(rs.getString("senha"));
@@ -151,28 +152,28 @@ public class PersistDatabase implements GesTADSDataBaseInterface {
 
     public void removeEmployee(Employee employee) {
 
-        long id = employee.getId();
-
         String sql = "delete from employee where id = ?";
 
         try {
 
             PreparedStatement pstm = src.model.repository.database.ConnectionDataBase.getCurrentConnection().prepareStatement(sql);
 
-            pstm.setLong(1, id);
+            pstm.setLong(1, employee.getId());
 
             pstm.execute();
 
         } catch (SQLException e) {
-            if(GesLogger.ISFULLLOGABLE || GesLogger.ISERRORLOGABLE)
+            if (GesLogger.ISFULLLOGABLE || GesLogger.ISERRORLOGABLE)
                 GesLogger.e(TAG, "Erro ao remover employee: " + e.getMessage());
+        } catch (Exception e) {
+            System.out.println(e);
         }
     }
 
     public void updateEmployee(Employee employee, Long id){
 
         String sql = "update employee set nome = ?, "
-                + "login = ?, senha = ?, cargo = ?, cpf = ?"
+                + "login = ?, cargo = ?,  cpf = ?"
                 + "where id = ?";
 
         try {
@@ -180,22 +181,22 @@ public class PersistDatabase implements GesTADSDataBaseInterface {
 
             pstm.setString(1, employee.getNome());
             pstm.setString(2, employee.getLogin());
-            pstm.setString(3, employee.getSenha());
-            pstm.setString(4, employee.getCargo());
-            pstm.setString(5, employee.getCpf());
-            pstm.setLong(6, id);
-
+            pstm.setString(3, employee.getCargo());
+            pstm.setString(4, employee.getCpf());
+            pstm.setLong(5, id);
             pstm.execute();
 
         } catch (SQLException e) {
             if(GesLogger.ISFULLLOGABLE || GesLogger.ISERRORLOGABLE)
                 GesLogger.e(TAG, "Erro ao atualizar employee: " + e.getMessage());
+        } catch (Exception e){
+            System.out.println(e);
         }
     }
 
     public List<String> getCargos() {
 
-        String sql = "select cargo from employee";
+        String sql = "select nome from cargo";
 
         List<String> cargos = new ArrayList<>();
 
@@ -204,13 +205,8 @@ public class PersistDatabase implements GesTADSDataBaseInterface {
 
             ResultSet rs = pstm.executeQuery();
 
-            while(rs.next()) {
-
-                Employee e = new Employee();
-
-                e.setCargo(rs.getString("cargo"));
-
-                cargos.add(String.valueOf(e));
+            while(rs.next()){
+                cargos.add(((rs.getString("nome"))));
             }
 
         } catch (SQLException e) {
@@ -222,31 +218,154 @@ public class PersistDatabase implements GesTADSDataBaseInterface {
     }
 
     public void setCargo(String position){
+       String sql = "insert into cargo(nome) values (?)";
+
+        try {
+            PreparedStatement pstm = src.model.repository.database.ConnectionDataBase.getCurrentConnection().prepareStatement(sql);
+            pstm.setString(1, position);
+            pstm.execute();
+
+        } catch (SQLException e) {
+            if(GesLogger.ISFULLLOGABLE || GesLogger.ISERRORLOGABLE)
+                GesLogger.e(TAG, "Erro ao inserir cargo: " + e.getMessage());
+        }
 
     }
 
     @Override
     public Product getProdutoPorId(Long id) {
+
+        String sql = "select * from produto where id = ?";
+
+        try {
+            PreparedStatement pstm = src.model.repository.database.ConnectionDataBase.getCurrentConnection().prepareStatement(sql);
+
+            pstm.setLong(1, id);
+
+            ResultSet rs = pstm.executeQuery();
+
+            Product p = null;
+
+            if(rs.next()) {
+                p = new Product();
+                p.setNome(rs.getString("nome"));
+                p.setSerialN(rs.getString("serialn"));
+                p.setFabricante(rs.getString("fabricante"));
+                p.setDescricao(rs.getString("descricao"));
+                p.setFabricacao(rs.getString("fabricacao"));
+                p.setValidade(rs.getString("validade"));
+            }
+
+            return p;
+
+        } catch (SQLException e) {
+            if(GesLogger.ISFULLLOGABLE || GesLogger.ISERRORLOGABLE)
+                GesLogger.e(TAG, "Erro ao buscar produto por id: " + e.getMessage());
+        }
+
         return null;
     }
 
     @Override
     public List<Product> getProdutos() {
-        return null;
+        String sql = "select * from produto";
+        List<Product> produtos = new ArrayList<>();
+
+        try {
+            PreparedStatement pstm = src.model.repository.database.ConnectionDataBase.getCurrentConnection().prepareStatement(sql);
+
+            ResultSet rs = pstm.executeQuery();
+
+            while(rs.next()) {
+
+                Product p = new Product();
+                p.setNome(rs.getString("nome"));
+                p.setSerialN(rs.getString("serialn"));
+                p.setFabricante(rs.getString("fabricante"));
+                p.setDescricao(rs.getString("descricao"));
+                p.setFabricacao(rs.getString("fabricacao"));
+                p.setValidade(rs.getString("validade"));
+
+                produtos.add(p);
+            }
+
+        } catch (SQLException e) {
+            if(GesLogger.ISFULLLOGABLE || GesLogger.ISERRORLOGABLE)
+                GesLogger.e(TAG, "Erro ao listar produtos: " + e.getMessage());
+        }
+
+        return produtos;
     }
 
     @Override
     public void setProduto(Product produto) {
+        String sql = "insert into produto (id, nome, serialn, fabricante, descricao, fabricacao, validade) values (?,?,?,?,?,?,?)";
 
+        try {
+            PreparedStatement pstm = src.model.repository.database.ConnectionDataBase.getCurrentConnection().prepareStatement(sql);
+
+            pstm.setLong(1, produto.getId());
+            pstm.setString(2, produto.getNome());
+            pstm.setString(3, produto.getSerialN());
+            pstm.setString(4, produto.getFabricante());
+            pstm.setString(5, produto.getDescricao());
+            pstm.setString(6, produto.getFabricacao());
+            pstm.setString(7, produto.getValidade());
+
+            pstm.execute();
+
+        } catch (SQLException e) {
+            if(GesLogger.ISFULLLOGABLE || GesLogger.ISERRORLOGABLE)
+                GesLogger.e(TAG, "Erro ao inserir produto: " + e.getMessage());
+        }
     }
 
     @Override
     public void editProduto(Product produto, Long id) {
+        String sql = "update produto set nome = ?, "
+                + "serialn = ?, fabricante = ?,  descricao = ?," +
+                "fabricacao = ?, validade = ?"
+                + "where id = ?";
 
+        try {
+            PreparedStatement pstm = src.model.repository.database.ConnectionDataBase.getCurrentConnection().prepareStatement(sql);
+
+            pstm.setString(1, produto.getNome());
+            pstm.setString(2, produto.getSerialN());
+            pstm.setString(3, produto.getFabricante());
+            pstm.setString(4, produto.getDescricao());
+            pstm.setString(5, produto.getFabricacao());
+            pstm.setString(6, produto.getValidade());
+            pstm.setLong(7, id);
+            pstm.execute();
+
+        } catch (SQLException e) {
+            if(GesLogger.ISFULLLOGABLE || GesLogger.ISERRORLOGABLE)
+                GesLogger.e(TAG, "Erro ao atualizar produto: " + e.getMessage());
+        } catch (Exception e){
+            System.out.println(e);
+        }
     }
 
     @Override
     public void removeProduto(Product produto) {
+
+        String sql = "delete from produto where id = ?";
+
+        try {
+
+            PreparedStatement pstm = src.model.repository.database.ConnectionDataBase.getCurrentConnection().prepareStatement(sql);
+
+            pstm.setLong(1, produto.getId());
+
+            pstm.execute();
+
+        } catch (SQLException e) {
+            if (GesLogger.ISFULLLOGABLE || GesLogger.ISERRORLOGABLE)
+                GesLogger.e(TAG, "Erro ao remover produto: " + e.getMessage());
+        } catch (Exception e) {
+            System.out.println(e);
+        }
 
     }
 
@@ -262,6 +381,35 @@ public class PersistDatabase implements GesTADSDataBaseInterface {
 
     @Override
     public Product getProdutoPorSerial(String serialNo) {
+
+        String sql = "select * from produto where serialn = ?";
+
+        try {
+            PreparedStatement pstm = src.model.repository.database.ConnectionDataBase.getCurrentConnection().prepareStatement(sql);
+
+            pstm.setString(1, serialNo);
+
+            ResultSet rs = pstm.executeQuery();
+
+            Product p = null;
+
+            if(rs.next()) {
+                p = new Product();
+                p.setNome(rs.getString("nome"));
+                p.setSerialN(rs.getString("serialn"));
+                p.setFabricante(rs.getString("fabricante"));
+                p.setDescricao(rs.getString("descricao"));
+                p.setFabricacao(rs.getString("fabricacao"));
+                p.setValidade(rs.getString("validade"));
+            }
+
+            return p;
+
+        } catch (SQLException e) {
+            if(GesLogger.ISFULLLOGABLE || GesLogger.ISERRORLOGABLE)
+                GesLogger.e(TAG, "Erro ao buscar produto por serialn: " + e.getMessage());
+        }
+
         return null;
     }
 
